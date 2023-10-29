0d0b2e02776c34bd32ba81f7c2b99f430ce5c6b8
Sulkhan <126507578+Sulkhans@users.noreply.github.com>
2023-10-29 17:36:57 +0400

    Pokedex

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 const Home = () => {
   return (
     <div className="h-screen flex justify-center items-center bg-neutral-200">
-      <div className="bg-[#e3350d] h-[32rem] w-80 rounded-md shadow-xl shadow-neutral-500 relative">
-        <div className="bg-[#be2c0b] h-12 rounded-t-md" />
+      <div className="bg-[#e3350d] h-[32rem] w-80 rounded-md shadow-xl shadow-neutral-500 relative bg-gradient-to-b from-[#e3350d] to-[#be2c0b] ...">
+        <div className="bg-[#be2c0b] h-12 rounded-t-md " />
         <div className=" absolute top-4 left-7 flex justify-center items-center w-14 h-14 rounded-full bg-neutral-300">
-          <div className="w-11 h-11 rounded-full bg-blue-500" />
+          <div className="w-11 h-11 rounded-full color" />
         </div>
         <div className="flex gap-2 absolute top-3 left-24">
           <div className="h-3 w-3 rounded-full shadow-inner bg-red-600" />
@@ -15,7 +16,7 @@ const Home = () => {
           <div className="h-3 w-3 rounded-full shadow-inner bg-lime-600" />
         </div>
         <div className="flex">
-          <div className="bg-[#be2c0b] h-10 w-2/5" />
+          <div className="bg-[#be2c0b] h-10 w-2/5 shadow-md" />
           <div
             style={{
               borderLeft: "2.5rem solid #be2c0b",
@@ -24,50 +25,48 @@ const Home = () => {
           />
         </div>
         <main className="m-7">
-          <div className="bg-neutral-300 h-60 rounded-md p-5">
-            <nav className="bg-sky-400 h-full p-2 flex flex-col gap-2 overflow-y-scroll text-white text-lg">
+          <div className="bg-neutral-300 h-64  rounded-md p-5 shadow-inner shadow-neutral-400">
+            <nav className="h-full p-2 grid grid-rows-2 grid-cols-2 gap-2 shadow-inner bg-gradient-to-b from-cyan-500 to-blue-500 ... font-semibold text-white tracking-wider">
               <Link
                 to="/Pokedex/Pokemon"
-                className=" hover:bg-neutral-300 px-4 py-3 bg-[#c32f27] rounded-sm"
+                className="bg-blue-200 bg-opacity-50 rounded-md flex justify-center items-center transition-all pokeball"
               >
-                Pokemon
+                <img src={logo} className="absolute opacity-20 w-16" />
+                <h1 className="z-10">Pokemon</h1>
               </Link>
               <Link
                 to="/Pokedex/Items"
-                className=" hover:bg-neutral-300 px-4 py-3 bg-black rounded-sm"
+                className="bg-blue-200 bg-opacity-50 rounded-md flex justify-center items-center transition-all pokeball"
               >
-                Items
+                <img src={logo} className="absolute opacity-20 w-16" />
+                <h1 className="z-10">Items</h1>
               </Link>
               <Link
                 to="/Pokedex/Moves"
-                className=" hover:bg-neutral-300 px-4 py-3 bg-black rounded-sm"
+                className="bg-blue-200 bg-opacity-50 rounded-md flex justify-center items-center transition-all pokeball"
               >
-                Moves
+                <img src={logo} className="absolute opacity-20 w-16" />
+                <h1 className="z-10">Moves</h1>
               </Link>
               <Link
                 to="/Pokedex/Locations"
-                className=" hover:bg-neutral-300 px-4 py-3 bg-black rounded-sm"
+                className="bg-blue-200 bg-opacity-50 rounded-md flex justify-center items-center transition-all pokeball"
               >
-                Locations
-              </Link>
-              <Link
-                to="/Pokedex/Locations"
-                className=" hover:bg-neutral-300 px-4 py-3 bg-black rounded-sm"
-              >
-                Locations
+                <img src={logo} className="absolute opacity-20 w-16" />
+                <h1 className="z-10">Locations</h1>
               </Link>
             </nav>
           </div>
           <div>
-            <div className="flex gap-5 mt-5 justify-center">
-              <div className="h-3 w-14 rounded-full bg-[#be2c0b]" />
-              <div className="h-3 w-14 rounded-full bg-sky-950" />
+            <div className="flex gap-4 mt-4 justify-center">
+              <div className="h-3 w-14 rounded-full bg-red-600 shadow-sm" />
+              <div className="h-3 w-14 rounded-full bg-sky-900 shadow-sm" />
             </div>
             <div className="flex gap-7">
-              <div className="w-11 h-11 rounded-full bg-neutral-900" />
-              <div className="w-24 bg-[#4fad60] rounded-md mt-7 mr-7" />
+              <div className="w-11 h-11 rounded-full bg-neutral-900 shadow-xl" />
+              <div className="w-24 rounded-md mt-7 mr-7 bg-green-600 shadow-sm shadow-neutral-600" />
               <div className="relative mt-3">
-                <div className="h-16 w-6 bg-neutral-900 rounded-md" />
+                <div className="h-16 w-6 bg-neutral-900 rounded-md shadow-xl" />
                 <div className="h-6 w-16 absolute top-5 -left-5 bg-neutral-900 rounded-md" />
               </div>
             </div>
