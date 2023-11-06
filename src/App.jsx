83173d8e9fef8147e5ad701cf02a7070b16f6c00
Sulkhan <126507578+Sulkhans@users.noreply.github.com>
2023-11-06 23:59:13 +0400

    Style change

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <>
       <Header />
-      <div className="min-h-screen ">
+      <div>
         <Outlet />
       </div>
     </>
--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -25,15 +25,15 @@ const Details = () => {
   }, []);
 
   return (
-    <main className="flex flex-col justify-center items-center m-6">
-      <div className="relative w-fit flex flex-col justify-center bg-gradient-to-b from-white from-50% to-neutral-200 to-95% ... rounded-md overflow-hidden p-6">
+    <main className="flex flex-col sm:flex-row sm:items-stretch justify-center items-center">
+      <div className="relative flex flex-col justify-between bg-gradient-to-b from-white from-50% to-neutral-200 to-95% ... overflow-hidden px-6 w-full">
         <button
           className="absolute right-3 top-3 z-20"
           onClick={() => setShiny(!shiny)}
         >
           <Shiny className="w-7 h-7 fill-amber-400" />
         </button>
-        <div className="w-72  z-10">
+        <div className="w-72 z-10 self-center m-4">
           {shiny ? (
             <img
               src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/${id}.png`}
@@ -45,16 +45,16 @@ const Details = () => {
           )}
         </div>
         <div>
-          <h1 className="text-3xl">{pokemon.name}</h1>
+          <h1 className="text-4xl relative z-10">{pokemon.name}</h1>
           <h2 className="text-neutral-400">#{id.toString().padStart(4, 0)}</h2>
           <img
             src={logo}
-            className="w-48 absolute -bottom-12 -right-12 opacity-40"
+            className="w-48 absolute -bottom-12 -right-12 opacity-40 hidden sm:block"
           />
         </div>
       </div>
-      <div className="self-start">
-        <div className="flex gap-2">
+      <div className="bg-neutral-200 rounded-md flex flex-col w-full px-6 py-4">
+        <div className="flex gap-2 items-center">
           {pokemon.types &&
             pokemon.types.map((obj, i) => (
               <p
@@ -67,14 +67,16 @@ const Details = () => {
               </p>
             ))}
         </div>
-        <div>
-          <h3>Abilities:</h3>
+        <div className="mt-6 bg-neutral-300 p-4 rounded-md">
+          <h3 className="text-2xl">Abilities</h3>
           <div>
             {pokemon.abilities &&
               pokemon.abilities.map((obj, i) => (
                 <p
                   key={i}
-                  className={`${obj.is_hidden ? "text-green-600" : ""}`}
+                  className={`${
+                    obj.is_hidden && "bg-neutral-400"
+                  } text-lg px-2`}
                 >
                   {format(obj.ability.name)}
                 </p>
@@ -82,19 +84,30 @@ const Details = () => {
           </div>
         </div>
         <div>
-          <h3>Stats:</h3>
-          {pokemon.stats &&
-            pokemon.stats.map((obj, i) => (
-              <p key={i}>
-                {format(obj.stat.name)}: {obj.base_stat}
-              </p>
-            ))}
+          <h3>Base stats</h3>
+          <div className="flex text-center justify-center mt-2">
+            {pokemon.stats &&
+              pokemon.stats.map((obj, i) => (
+                <div key={i} className="flex flex-col items-center w-9">
+                  <div
+                    className="flex items-end justify-center bg-neutral-300 mb-2"
+                    style={{ height: "255px" }}
+                  >
+                    <div
+                      className="bg-neutral-500"
+                      style={{
+                        width: "30px",
+                        height: `${obj.base_stat}px`,
+                      }}
+                    />
+                  </div>
+                  <p>{obj.base_stat}</p>
+                  <p className="text-xs">{format(obj.stat.name)}</p>
+                </div>
+              ))}
+          </div>
         </div>
       </div>
-      {/* <div>
-        {pokemon.moves &&
-          pokemon.moves.map((obj, i) => <p key={i}>{obj.move.name}</p>)}
-      </div> */}
     </main>
   );
 };
